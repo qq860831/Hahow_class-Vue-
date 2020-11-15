6904fc6288a79b46340416fde1797d1009ce5f80
Ian <qq860831@gmail.com>
2020-11-15 17:22:15 +0800

    123

---
+++ all.js
@@ -0,0 +1,18 @@
+var apiurl="https://awiclass.monoame.com/api/command.php?type=get&name=hahowdata";
+
+var vm = new Vue({
+    el:"#app",
+    data:{
+        classdatas:[]
+    }
+});
+
+$.ajax({
+    url:"https://awiclass.monoame.com/api/command.php?type=get&name=hahowdata",
+    success:function(res){
+        var obj=JSON.parse(res);
+        console.log(obj);
+        vm.classdatas=obj;
+        Vue.set(vm,"classdatas",obj);
+    }
+});
